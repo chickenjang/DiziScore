6bf668d9297229b7e9684b0eae5ae274c695f75e
jiwon <scorpiusjwj@gmail.com>
2022-02-15 11:41:27 +0900

    [feat] 인원수,이름 입력받아 session storage에 저장

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
 function Game() {
-  return <div>게임 페이지 입니다</div>;
+  const [stage, setStage] = useState(0);
+
+  function handleNumSubmit(event) {
+    event.preventDefault();
+    window.sessionStorage.setItem(
+      "numOfPlayers",
+      JSON.stringify(event.target.numOfPlayers.value)
+    );
+    setStage(1);
+  }
+
+  function handleNameSubmit(event) {
+    event.preventDefault();
+    const players = [...event.target.player].map((item) => item.value);
+    window.sessionStorage.setItem("players", JSON.stringify(players));
+    setStage(2);
+  }
+
+  const numOfPlayers = window.sessionStorage.getItem("numOfPlayers");
+
+  const inputNames = [];
+  let i = 1;
+  while (i <= JSON.parse(numOfPlayers)) {
+    inputNames.push(<input type="text" key={i} name="player" />);
+    i++;
+  }
+
+  return (
+    <div>
+      <h2>플레이 할 인원 수를 입력하세요</h2>
+      {stage === 0 && (
+        <form onSubmit={handleNumSubmit}>
+          <input type="number" min={2} max={4} name="numOfPlayers" />
+          <input type="submit" value="다음" />
+        </form>
+      )}
+      {stage === 1 && (
+        <form onSubmit={handleNameSubmit}>
+          {inputNames}
+          <input type="submit" value="다음" />
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default Game;
