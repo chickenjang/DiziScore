50ac4d798936bd1093a91217a6695f639986681c
jiwon <scorpiusjwj@gmail.com>
2022-02-16 13:11:08 +0900

    [feat] 상세보기 페이지 기본기능 구현

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,5 +1,20 @@
+import { useParams } from "react-router-dom";
+import Grade from "../components/Grade";
+
 function Post() {
-  return <div>상세페이지</div>;
+  const { id } = useParams();
+  // TODO: id에 따라 글 불러오기
+  return (
+    <div>
+      <div>
+        <div>제목</div>
+        {id}
+        <div>내용</div>
+        {id}
+        <Grade grade={2} />
+      </div>
+    </div>
+  );
 }
 
 export default Post;
