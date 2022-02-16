7b004cd95618169631364331c4e83a84aca8daa6
chickenjang <86708793+chickenjang@users.noreply.github.com>
2022-02-16 13:20:33 +0900

    [feat] 상세보기 페이지 구현 (#7)
    * [feat] 상세보기 페이지 기본기능 구현
    
    * [fix] 상세보기 페이지 목록 변경

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,5 +1,20 @@
+import { useParams } from "react-router-dom";
+import Grade from "../components/Grade";
+
 function Post() {
-  return <div>상세페이지</div>;
+  const { id } = useParams();
+  // TODO: id에 따라 글 불러오기
+  return (
+    <div>
+      <div>
+        <div>제목</div>
+        <div>작성자</div>
+        <div>작성시간</div>
+        <div>내용</div>
+        <Grade grade={2} />
+      </div>
+    </div>
+  );
 }
 
 export default Post;
