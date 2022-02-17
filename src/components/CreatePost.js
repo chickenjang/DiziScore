e4143eec8d53d710d8efae72e5b594be77e37297
jiwon <scorpiusjwj@gmail.com>
2022-02-17 12:26:48 +0900

    [feat] 게시글 작성 api통신 및 db등록 구현

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -7,9 +7,24 @@ function CreatePost() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // TODO: API통신하여 DB에 추가
-    // console.log(event.target);
-    // console.log(grade);
+
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const body = event.target.body.value;
+
+    fetch("http://localhost:3001/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        author: author,
+        body: body,
+        createdAt: Date.now(),
+        grade: grade,
+      }),
+    });
   }
 
   // TODO: 취소버튼, 등록버튼 리다이렉션 구현
@@ -17,6 +32,8 @@ function CreatePost() {
     <form onSubmit={handleSubmit}>
       <label htmlFor="title">제목</label>
       <input type="text" name="title" id="title" />
+      <label htmlFor="author">작성자</label>
+      <input type="text" name="author" id="author" />
       <label htmlFor="body">내용</label>
       <textarea
         name="body"
