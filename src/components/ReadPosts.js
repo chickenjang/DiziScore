8754f1008876c7c206a2bbbee271954de2794b97
jiwon <scorpiusjwj@gmail.com>
2022-02-17 12:09:13 +0900

    [feat] 게시글목록 읽어오기 구현 및 더미데이터 제거

--- src/components/ReadPosts.js
+++ src/components/ReadPosts.js
@@ -1,18 +1,33 @@
-import { postsData } from "../dummy/db";
+// import { postsData } from "../dummy/db";
 import Grade from "./Grade";
 import PastTime from "./PastTime";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function ReadPosts() {
-  const posts = postsData.map((post) => (
-    // css생성 후 style속성 삭제 필요
-    <Link to={`/posts/${post.id}`} key={post.id} style={{ display: "flex" }}>
-      <div>{post.title}</div>
-      <Grade grade={post.grade} />
-      <div>{post.author}</div>
-      <PastTime createdAt={post.createdAt} />
-    </Link>
-  ));
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/posts")
+      .then((response) => response.json())
+      .then((result) =>
+        setPosts(() =>
+          result.map((post) => (
+            // css생성 후 style속성 삭제 필요
+            <Link
+              to={`/posts/${post.id}`}
+              key={post.id}
+              style={{ display: "flex" }}
+            >
+              <div>{post.title}</div>
+              <Grade grade={post.grade} />
+              <div>{post.author}</div>
+              <PastTime createdAt={post.createdAt} />
+            </Link>
+          ))
+        )
+      );
+  }, [posts]);
 
   return <div>{posts}</div>;
 }
