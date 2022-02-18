b21b75b65ca14822de43ecf8de2d7b02b9e0b0cb
jiwon <scorpiusjwj@gmail.com>
2022-02-18 12:36:45 +0900

    [style] 게임 기본 정보입력 스타일 구현

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,6 @@
 import Play from "../components/Play";
 import { useState } from "react";
+import "./Game.css";
 
 function Game() {
   const [stage, setStage] = useState(0);
@@ -25,41 +26,68 @@ function Game() {
   const inputNames = [];
   let i = 1;
   while (i <= numOfPlayers) {
-    inputNames.push(<input type="text" key={i} name="player" />);
+    inputNames.push(
+      <input
+        type="text"
+        key={i}
+        className="game-form-input"
+        name="player"
+        required
+      />
+    );
     i++;
   }
 
   return (
     <div>
       {stage === 0 && (
-        <div>
-          <h2>플레이 할 인원 수를 입력하세요</h2>
-          <form onSubmit={handleNumSubmit}>
+        <div className="game">
+          <h2 className="game-title">플레이 할 인원 수를 입력하세요</h2>
+          <form onSubmit={handleNumSubmit} className="game-form">
             <input
+              className="game-form-input"
               type="number"
               min={2}
               max={4}
               name="numOfPlayers"
               placeholder="2~4까지 숫자를 입력하세요"
+              required
             />
-            <input type="submit" value="다음" />
+            <input className="game-form-button" type="submit" value="다 음" />
           </form>
         </div>
       )}
       {stage === 1 && (
-        <div>
-          <h2>플레이들의 이름을 입력하세요</h2>
-          <form onSubmit={handleNameSubmit}>
+        <div className="game">
+          <h2 className="game-title">플레이들의 이름을 입력하세요</h2>
+          <form onSubmit={handleNameSubmit} className="game-form">
             {inputNames}
-            <button onClick={() => setStage(stage - 1)}>이전</button>
-            <input type="submit" value="다음" />
+            <div className="game-form-btns">
+              <button
+                className="game-form-button"
+                onClick={() => setStage(stage - 1)}
+              >
+                이전
+              </button>
+              <input type="submit" value="다음" className="game-form-button" />
+            </div>
           </form>
         </div>
       )}
       {stage === 2 && (
-        <div>
-          <h2>게임을 시작할까요?</h2>
-          <button onClick={() => setStage(3)}>시작하기</button>
+        <div className="game">
+          <h2 className="game-title">게임을 시작해볼까요?</h2>
+          <div className="game-btns">
+            <button
+              className="game-form-button"
+              onClick={() => setStage(stage - 1)}
+            >
+              이전
+            </button>
+            <button className="game-form-button" onClick={() => setStage(3)}>
+              시작하기
+            </button>
+          </div>
         </div>
       )}
       {stage === 3 && <Play numOfPlayers={numOfPlayers} players={players} />}
