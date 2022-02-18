37c7f09e55caa1d4f0066df4afa1d97151b2fed6
jiwon <scorpiusjwj@gmail.com>
2022-02-18 10:46:13 +0900

    [style] 폰트 반영

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
+import "./Main.css";
+import postImg from "../static/images/post.png";
+import gameImg from "../static/images/game.png";
 
 function Main() {
   return (
     <div>
-      <h2>어떤 걸 하시겠어요?</h2>
-      <Link to="/posts">게시판</Link>
-      <Link to="/game">게임</Link>
+      <h2 className="title">어떤 걸 하시겠어요?</h2>
+      <div className="body">
+        <Link to="/posts" className="body-item">
+          <p>게시판</p>
+          <img src={postImg} alt="게시판이미지" />
+        </Link>
+        <Link to="/game" className="body-item">
+          <p>게임하기</p>
+          <img src={gameImg} alt="게임이미지" />
+        </Link>
+      </div>
     </div>
   );
 }
