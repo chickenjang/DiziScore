1e16e8930a1a0c6f436ba55ec2998d0197a41747
jiwon <scorpiusjwj@gmail.com>
2022-02-20 20:02:02 +0900

    [fix] 메인 라우터주소만 변경하는것으로 재수정

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <Routes>
       <Route path="/DiziScore" element={<Main />} />
-      <Route path="/DiziScore/posts" element={<Posts />} />
-      <Route path="/DiziScore/posts/:id" element={<Post />} />
-      <Route path="/DiziScore/posts/create" element={<CreatePost />} />
-      <Route path="/DiziScore/game" element={<Game />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/posts/:id" element={<Post />} />
+      <Route path="/posts/create" element={<CreatePost />} />
+      <Route path="/game" element={<Game />} />
     </Routes>
   );
 }
